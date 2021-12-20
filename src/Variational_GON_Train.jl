b352042ed07bd8def3a9b2016741388e152c86c4
Barışcan <bbozkurt15@ku.edu.tr>
2021-12-20 12:32:21 +0300

    Variational Gon Train code modified: load coil dataset added

--- src/Variational_GON_Train.jl
+++ src/Variational_GON_Train.jl
@@ -15,6 +15,7 @@ using ImageTransformations
 using Statistics
 using Memento
 using NPZ
+using Random
 # using Interpolations
 atype=(CUDA.functional() ? KnetArray{Float32} : Array{Float32})
 
@@ -112,7 +113,20 @@ elseif dataset_name == "cifar"
     xtst,_ = CIFAR10.testdata()
     xtrn = Array{Float64, 4}(xtrn)
     xtst = Array{Float64, 4}(xtst)
-#     println("No implemented yet")
+elseif dataset_name == "coil"
+    coil_path = "Data/coil20"
+    coil = load_coil_dataset(coil_path);
+    nc = 1
+    dataset_size = size(coil,3)
+    trn_perc = 0.8
+    trn_size = Int(dataset_size * trn_perc)
+    random_permutation = randperm(dataset_size)
+    trn_idx = random_permutation[1:trn_size]
+    tst_idx = random_permutation[trn_size + 1:end]
+    xtrn = coil[:,:,trn_idx]
+    xtrn = resize_gray_image_tensor(xtrn, 1/4)
+    xtst = coil[:,:,tst_idx];
+    xtst = resize_gray_image_tensor(xtst, 1/4)
 end
 
 batch_size = 64
